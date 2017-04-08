4c11f16f5b0b99c8f9279e9101bfe80ea287fab6
wshibiao <790207553@qq.com>
2017-04-08 15:11:36 +0800

    修正update操作的错误

--- src/main/java/com/express/dao/ExpressDao.java
+++ src/main/java/com/express/dao/ExpressDao.java
@@ -13,14 +13,19 @@ public interface ExpressDao {
      * 录入快递信息
      * @param express
      */
-    public void insertExpress(Express express);
+    public void insertExpressInfo(Express express);
 
+    /**
+     * 更新快递信息
+     * @param express
+
+     */
     public void updateExpressInfo(Express express);
     /**
      * 查询未领取的快递信息
      * @return
      */
-    public Express getExpressInfoById(Long id);
+    public Express getExpressInfoById(@Param("id") Long id);
     /**
      * 通过手机号和快递号查询快递的信息
      * @param contact
